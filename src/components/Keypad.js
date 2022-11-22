a25e59a43ec439a8177049ddcdc847469e70d682
Hala AlArid <halaalarid99@gmail.com>
2022-11-22 13:26:02 +0200

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    function handleChange(event) {
+        // console.log(`${event.target.name}: ${event.target.value}`);
+        console.log("Entering password...")
+      }
+
     return (
-        <div></div>
+        <div>
+        <input
+        type="password"
+        onChange={handleChange}
+        placeholder="Enter password..."
+      />
+        </div>
     )
 }
 
